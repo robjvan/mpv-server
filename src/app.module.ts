1ff5e2c122545591aa1fefa8e3a8e80c85e831de
Rob Vandelinder <robjvan@gmail.com>
2025-05-04 18:01:57 -0230

    Sorted imports for clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,34 @@
 import { Module } from '@nestjs/common';
+
+import { AdminDashboardModule } from './modules/admin-dashboard/admin-dashboard.module';
+import { AppointmentsModule } from './modules/appointments/appointments.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
-import { AppointmentsModule } from './modules/appointments/appointments.module';
-import { PetsModule } from './modules/pets/pets.module';
-import { PaymentsModule } from './modules/payments/payments.module';
-import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
-import { MailModule } from './modules/mail/mail.module';
-import { LocationsModule } from './modules/locations/locations.module';
-import { AdminDashboardModule } from './modules/admin-dashboard/admin-dashboard.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { DocumentsModule } from './modules/documents/documents.module';
-import { ScheduleModule } from '@nestjs/schedule';
+import { CountryModule } from './modules/country/country.module';
 import { DatabaseModule } from './modules/database/database.module';
+import { DocumentsModule } from './modules/documents/documents.module';
+import { MailModule } from './modules/mail/mail.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { PetsModule } from './modules/pets/pets.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
-    DatabaseModule,
-    ScheduleModule.forRoot(),
-    UsersModule,
-    AppointmentsModule,
-    PetsModule,
-    PaymentsModule,
-    SubscriptionsModule,
-    MailModule,
-    LocationsModule,
     AdminDashboardModule,
+    AppointmentsModule,
     AuthModule,
+    CountryModule,
+    DatabaseModule,
     DocumentsModule,
+    MailModule,
+    PaymentsModule,
+    PetsModule,
+    ScheduleModule.forRoot(),
+    SubscriptionsModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
