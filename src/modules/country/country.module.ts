97515510f9f4cba9839dfa7b7b89ecf26ee9664c
Rob Vandelinder <robjvan@gmail.com>
2025-05-07 11:59:02 -0230

    Added missing import

--- src/modules/country/country.module.ts
+++ src/modules/country/country.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CountryService } from './country.service';
 import { CountryController } from './country.controller';
 import { countryProviders } from './country.providers';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [CountryController],
   providers: [CountryService, ...countryProviders],
 })
